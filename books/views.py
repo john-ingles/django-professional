10e975eb6f5e572dc9f9cd40c7db9b827a81f7e5
John Ingles <john.kenneth.ingles@gmail.com>
2022-08-25 22:44:49 -0400

    Make search results login required

--- books/views.py
+++ books/views.py
@@ -20,7 +20,7 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     permission_required = "books.special_status"
 
 
-class SearchResultsListView(ListView):
+class SearchResultsListView(LoginRequiredMixin, ListView):
     model = Book
     context_object_name = "book_list"
     template_name = "books/search_results.html"
